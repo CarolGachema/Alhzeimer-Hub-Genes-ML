345668ac3f30dc6ce01a86ebf565dc4060a9ccb9
Caroline <123540025+CarolGachema@users.noreply.github.com>
2026-07-02 13:47:53 +0300

    Update network_analysis.R

--- scripts/network_analysis.R
+++ scripts/network_analysis.R
@@ -40,7 +40,7 @@ library(ggraph)         # For network visualization using ggplot2
 
 
 # Set Working Directory
-setwd("C:/Users/Salome/Documents/MY PROJECTS/Alhzeimer-Hub-Genes-ML")
+setwd("C:/Users/Gachema/Documents/MY PROJECTS/Alhzeimer-Hub-Genes-ML")
 
 
 # Load GEO Dataset
